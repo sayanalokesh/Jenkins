81e00530e128f1aa61eaff50ff475b785d198ae3
Sayana Lokesh <105637305+sayanalokesh@users.noreply.github.com>
2023-11-23 21:23:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 
@@ -15,4 +16,6 @@ def contactUs():
     return 'this is contact us page'
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+    # Use WSGI server (such as gevent) to run Flask app in the background
+    http_server = WSGIServer(('0.0.0.0', 3000), app)
+    http_server.serve_forever()
